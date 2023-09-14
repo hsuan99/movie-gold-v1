b40638f84053b0ce3b925d19c9706deb99497f5f
Chia-hsuan Liu <liublanca1109@gmail.com>
2023-09-14 14:28:54 -0700

    Changes to be committed: 	modified:   package.json 	modified:   src/App.js 	new file:   src/api/axiosConfig.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig';
+import {useState, useEffect} from 'react';
 
 function App() {
+
+  const[movies, setMovies]= useState();
+
+  const getMovies = async() => {
+
+    try {
+
+      const response = await api.get("/api/v1/movies");
+      console.log(response.data);
+      setMovies(response.data);
+
+    } catch(err) {
+      console.log(err)   
+    }
+    
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +40,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
