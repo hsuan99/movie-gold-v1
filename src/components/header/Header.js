d8fd9e79f98b854d1bf2dd37a1581ea9dcc16441
Chia-hsuan Liu <liublanca1109@gmail.com>
2023-09-15 14:32:47 -0700

    new file:   src/components/header/Header.js 	modified:   src/components/hero/Hero.js     Add styling to headers: note the register function is not implemented.     It's purely for aesthetic purpose.

---
+++ src/components/header/Header.js
@@ -0,0 +1,36 @@
+// Font components
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
+// Bootstrap componenets
+import { Button } from "react-boostrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+        <Container fluid>
+            <Navbar.Brand href="/" style={{"color": "gold"}}>
+                <FontAwesomeIcon icon={faVideoSlash}/>Gold
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collpase id="navbarScroll">
+                <Nav
+                    className="me-autor my-2 my-lg-0"
+                    style={{maxHeight: "100px"}}
+                    navbarScroll
+                >
+                    <NavLink className="nav-link" to="/">Home</NavLink>
+                    <NavLink className="nav-link" to="/watchlist">Watch List</NavLink>
+                </Nav>
+                <Button variant="outline-info" className="me-2">Login</Button>
+                <Button variant="outline-info">Regsiter</Button>
+            </Navbar.Collpase>
+        </Container>
+    </Navbar>
+  )
+}
+
+export default Header
